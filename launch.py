981f1b53ae631d3f19462b90f9f15a20a9152628
Luis Oala <luis.oala@online.de>
2021-08-14 14:55:34 +0200

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,8 +1,13 @@
-import gradio
+import gradio as gr
+import numpy as np
 
-def hello(inp):
-  return "Hello {}!".format(inp)
+def sepia(img):
+  sepia_filter = np.array([[.393, .769, .189],
+                           [.349, .686, .168],
+                           [.272, .534, .131]])
+  sepia_img = img.dot(sepia_filter.T)
+  sepia_img /= sepia_img.max()                          
+  return sepia_img
 
-io = gradio.Interface(fn=hello, inputs='text', outputs='text', title='Hello World', 
-    description='The simplest Hosted interface.')  
-io.launch()
+iface = gr.Interface(sepia, gr.inputs.Image(shape=(200, 200)), "image")
+iface.launch()
